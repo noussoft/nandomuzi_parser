cf7d0cf49ca1521f1e4d49d5244c576d7bbc2879
noussoft <dmitry.lomash@gmail.com>
2016-10-14 00:48:50 +0400

    Added dialog infomation about job progress

--- nandomuzi_parser.py
+++ nandomuzi_parser.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import urllib.request
 from urllib.request import urlretrieve
 from bs4 import BeautifulSoup
@@ -15,8 +16,10 @@ def parse(html):
     soup = BeautifulSoup(html, "html.parser")
     shoes_list = soup.find('ul', class_="shoes")
     items = shoes_list.find_all('li')
+    print_info("Parsing (up to {}):".format(len(items)))
     result = []
     for i, item in enumerate(items):
+        print_info(i)
         links = item.find_all('a')
         images_page = get_html(URL + links[0]['href'])
         result.append({
@@ -36,7 +39,9 @@ def get_images(html):
     return result
     
 def save(data):
+    print_info("\nSaving (up to {}):".format(len(data)))
     for i, item in enumerate(data):
+        print_info(i)
         path = os.path.join(OUTPUT_DIR, item['sku']) 
         if not os.path.exists(path):
             os.makedirs(path)
@@ -44,8 +49,13 @@ def save(data):
             file_to_save = image.rsplit('/')[2]
             urlretrieve(URL + image, os.path.join(path, file_to_save))
 
+def print_info(info):
+    print(info, end=" ")
+    sys.stdout.flush()
+
 def main():
     save(parse(get_html(URL)))
+    print("\nJob is done")
 
 if __name__ == '__main__':
     main()
